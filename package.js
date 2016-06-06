0912a8804989b7a2ec3e786f1471e50afa444445
Simon Fridlund <simon.r.k.fridlund@gmail.com>
2016-06-06 22:26:28 +0200

    Add package.js and package.json

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  documentation: 'README.md',
+  git: 'https://github.com/activeroute/flow-router.git',
+  name: 'activeroute:flow-router',
+  summary: 'Flow-Router adapter for active-route.',
+  version: '0.0.1',
+});
+
+Package.onUse(function onUse(api) {
+  api.versionsFrom('1.3.2.4');
+
+  api.use('ecmascript');
+
+  api.use([
+    'activeroute:core@0.0.1',
+    'kadira:flow-router@2.12.1',
+  ]);
+});
