a1f8c9c27457f2739644e9ff68b7e33f9fcf4b25
Simon Fridlund <simon@fridlund.email>
2017-01-21 01:01:27 +0100

    Add eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+// @flow
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    meteor: true,
+    node: true,
+    'shared-node-browser': true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:meteor/recommended',
+  ],
+  parser: 'babel-eslint',
+  plugins: [
+    'meteor',
+    'no-use-extend-native',
+    'prefer-object-spread',
+  ],
+  rules: {
+    'import/extensions': ['error', 'never'],
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': ['error', {
+      ignore: ['^meteor/'],
+    }],
+    'max-len': ['error', 120, 2, {
+      ignoreComments: false,
+      ignoreRegExpLiterals: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignoreUrls: true,
+    }],
+    'max-params': ['warn', 1],
+    'no-param-reassign': ['warn', {
+      props: true,
+    }],
+    'no-use-extend-native/no-use-extend-native': 'error',
+    'prefer-arrow-callback': ['error', {
+      allowNamedFunctions: true,
+    }],
+    'prefer-object-spread/prefer-object-spread': 'error',
+    'sort-keys': 'warn',
+    'sort-vars': 'warn',
+  },
+};
